d573e0f6b6c964c6eef124ab79c0fe8be8c0b1b2
Woody Jo <140256930+hyunuzo@users.noreply.github.com>
2024-04-27 22:24:09 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -14,9 +14,17 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import telegram
+import asyncio
+from telegram import Update
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, MessageHandler
+import time
+from datetime import datetime
 
 LOGGER = get_logger(__name__)
-
+CHAT_ID = '5506745859'
+TELE_TOKEN = '6438318825:AAE9HGapmTQ853woseydqfunNnMak0RRNs0'
+NOW_TIME = datetime.now().time()
 
 def run():
     st.set_page_config(
@@ -35,6 +43,21 @@ def run():
      
     st.image('img/fest_img.jpg')
 
+async def msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # 이미지 경로
+    image_path = "img/fest_img.jpg"
+    # 이미지 파일을 읽어 들여 채팅방으로 전송
+    with open(image_path, "rb") as image_file:
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=image_file, caption="서버에서 보낸 이미지 파일입니다.")
+
 
 if __name__ == "__main__":
     run()
+     # 챗봇 application 인스턴스 생성
+    application = ApplicationBuilder().token(TELE_TOKEN).build()
+    # 핸들러 생성
+    msg_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), msg)
+    # 핸들러 추가
+    application.add_handler(msg_handler)
+    # 폴링 방식으로 실행
+    application.run_polling()
