70f242ce4b1fcd1ff0766a1c19ddd662810d56f1
Woody Jo <gusdn726@gmail.com>
2024-05-04 18:16:37 +0000

    modified:   pages/2_folium_marker.py

--- pages/2_folium_marker.py
+++ pages/2_folium_marker.py
@@ -7,6 +7,10 @@ from io import StringIO
 import geopandas as gpd
 from shapely.geometry import Polygon, MultiPolygon, Point
 from shapely.geometry import Point
+import json
+from streamlit_folium import folium_static
+
+
 
 path_csv = 'img/국토교통부_전국 버스정류장 위치정보1_20231016.csv'
 bus_stop = pd.read_csv(path_csv)
@@ -24,8 +28,14 @@ st.write(StringIO(uploaded_file.getvalue().decode("utf-8")))
 
 if uploaded_file is not None:
     gdf = gpd.read_file(uploaded_file)
+    geo_str = json.load(open(uploaded_file,encoding='utf-8'))
 else:
     st.write("폴리곤 파일을 업로드해주세요.")
+
+m = folium.Map(location=[35.164567,-230.937195], zoom_start=6)
+folium.Choropleth(    geo_data = geo_str).add_to(m)
+
+st_map = folium_static(m, width = 1100, height=500)
 
 #     # To read file as bytes:
 #     bytes_data = uploaded_file.getvalue()
@@ -42,12 +52,12 @@ else:
 #     # Can be used wherever a "file-like" object is accepted:
 #     dataframe = pd.read_csv(uploaded_file)
 #     st.write(dataframe)
-submit_button = st.form_submit_button(label='조회')
+# submit_button = st.form_submit_button(label='조회')
 
-if submit_button:
-    bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
-    m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
-    for idx, row in bs_poly.iterrows():
-        popup = f"Name: {row['정류장명']}"  # 마커 팝업에 표시할 정보 설정
-        folium.Marker(location=[row.geometry.y, row.geometry.x],popup=popup).add_to(m1)
-    st_folium(m1, width=1000, height=500)
+# if submit_button:
+#     bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
+#     m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
+#     for idx, row in bs_poly.iterrows():
+#         popup = f"Name: {row['정류장명']}"  # 마커 팝업에 표시할 정보 설정
+#         folium.Marker(location=[row.geometry.y, row.geometry.x],popup=popup).add_to(m1)
+#     st_folium(m1, width=1000, height=500)
