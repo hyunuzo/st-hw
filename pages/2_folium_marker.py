65b5f07cdea3ca62bfb6018269da43330fe0c53e
Woody Jo <gusdn726@gmail.com>
2024-05-04 18:25:22 +0000

    modified:   pages/2_folium_marker.py

--- pages/2_folium_marker.py
+++ pages/2_folium_marker.py
@@ -17,7 +17,7 @@ path_geo = 'img/s.geojson'
 bus_stop = pd.read_csv(path_csv)
 geometry = [Point(xy) for xy in zip(bus_stop['경도'],bus_stop['위도'])]
 gdf_bs = gpd.GeoDataFrame(bus_stop,geometry=geometry,crs='epsg:4326')
-
+gdf = gpd.read_file(path_geo)
 
 
 st.set_page_config(layout="wide")
@@ -54,12 +54,12 @@ st_map = folium_static(m, width = 1100, height=500)
 #     # Can be used wherever a "file-like" object is accepted:
 #     dataframe = pd.read_csv(uploaded_file)
 #     st.write(dataframe)
-# submit_button = st.form_submit_button(label='조회')
+submit_button = st.form_submit_button(label='조회')
 
-# if submit_button:
-#     bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
-#     m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
-#     for idx, row in bs_poly.iterrows():
-#         popup = f"Name: {row['정류장명']}"  # 마커 팝업에 표시할 정보 설정
-#         folium.Marker(location=[row.geometry.y, row.geometry.x],popup=popup).add_to(m1)
-#     st_folium(m1, width=1000, height=500)
+if submit_button:
+    bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
+    m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
+    for idx, row in bs_poly.iterrows():
+        popup = f"Name: {row['정류장명']}"  # 마커 팝업에 표시할 정보 설정
+        folium.Marker(location=[row.geometry.y, row.geometry.x],popup=popup).add_to(m1)
+    st_folium(m1, width=1000, height=500)
