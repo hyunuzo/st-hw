b97bc683bef4fb9b2067e9f9aed91bbe8779c5f3
Woody Jo <gusdn726@gmail.com>
2024-05-04 18:11:48 +0000

    modified:   pages/2_folium_marker.py

--- pages/2_folium_marker.py
+++ pages/2_folium_marker.py
@@ -18,11 +18,14 @@ gdf_bs = gpd.GeoDataFrame(bus_stop,geometry=geometry,crs='epsg:4326')
 st.set_page_config(layout="wide")
 
 uploaded_file = st.file_uploader("Choose a file")
+submit_button = st.form_submit_button(label='조회')
 st.write(uploaded_file.getvalue())
 st.write(StringIO(uploaded_file.getvalue().decode("utf-8")))
 
 if uploaded_file is not None:
     gdf = gpd.read_file(uploaded_file)
+else:
+    st.write("폴리곤 파일을 업로드해주세요.")
 
 #     # To read file as bytes:
 #     bytes_data = uploaded_file.getvalue()
@@ -40,7 +43,7 @@ if uploaded_file is not None:
 #     dataframe = pd.read_csv(uploaded_file)
 #     st.write(dataframe)
 
-submit_button = st.form_submit_button(label='조회')
+
 if submit_button:
     bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
     m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
