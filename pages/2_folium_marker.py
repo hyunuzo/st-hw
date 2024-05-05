71d38010f003f7ee4c173d4b0bd823cef21ea19f
Woody Jo <gusdn726@gmail.com>
2024-05-05 11:21:58 +0000

    modified:   pages/2_folium_marker.py

--- pages/2_folium_marker.py
+++ pages/2_folium_marker.py
@@ -65,7 +65,7 @@ with st.sidebar.form(key='search_form'):
 
 
 
-st_m = folium_static(m1, width=1000, height=500)
+st_m = st_folium(m1, width=1000, height=500)
 if df_bs_poly is not None:
     st.write(df_bs_poly)
 else:
