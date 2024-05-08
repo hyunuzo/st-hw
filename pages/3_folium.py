1a5216a805b5ceba0381a491ae79895d3de185c2
Woody Jo <gusdn726@gmail.com>
2024-05-08 13:13:59 +0000

    modified:   pages/3_folium.py

--- pages/3_folium.py
+++ pages/3_folium.py
@@ -62,7 +62,7 @@ with st.container():
             with c1:
                 uploaded_file = st.file_uploader("폴리곤파일(*.geojson)을 업로드해주세요.",type='geojson')
             with c2:
-                button = st.form_submit_button(label='🔎 **조     회** 🔎')
+                button = st.form_submit_button(label='🔎 **조______회** 🔎')
                 b1 = st.form_submit_button("🔄 **영역 재설정** 🔄")
             if button:
                 if uploaded_file is not None:
