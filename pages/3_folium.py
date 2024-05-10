cd4350a2eddf40c604e9f71b9aae11f466faca8c
Woody Jo <gusdn726@gmail.com>
2024-05-10 12:55:45 +0000

    modified:   pages/3_folium.py

--- pages/3_folium.py
+++ pages/3_folium.py
@@ -68,16 +68,17 @@ with col2:
 if bt_search:
     if uploaded_file is not None:
         gdf = gpd.read_file(uploaded_file)
-        bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
-        df_bs_poly = pd.DataFrame(bs_poly.drop(columns='geometry'))
-        m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
-        folium.plugins.Fullscreen(position="topright",title="전체화면",title_cancel="나가기",force_separate_button=True).add_to(m1)
-        folium.GeoJson(data=gdf['geometry'],).add_to(m1)
-        # folium.GeoJson(data=gdf['geometry'],style_function=lambda feature: {'fillColor': 'yellow','color': 'yellow'}).add_to(m1)
-        for idx, row in bs_poly.iterrows():
-            popup = folium.Popup("<b>정류장명 : </b>" + f"{row['정류장명']}",max_width=300) # 마커 팝업에 표시할 정보 설정
-            tooltip = f"정류장번호: {row['정류장번호']}"
-            folium.Circle(location=[row.geometry.y, row.geometry.x],radius=10,fill=True,fill_opacity=0.8,popup=popup,tooltip=tooltip).add_to(m1)
+        if bt_search:
+            bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
+            df_bs_poly = pd.DataFrame(bs_poly.drop(columns='geometry'))
+            m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
+            folium.plugins.Fullscreen(position="topright",title="전체화면",title_cancel="나가기",force_separate_button=True).add_to(m1)
+            folium.GeoJson(data=gdf['geometry'],).add_to(m1)
+            # folium.GeoJson(data=gdf['geometry'],style_function=lambda feature: {'fillColor': 'yellow','color': 'yellow'}).add_to(m1)
+            for idx, row in bs_poly.iterrows():
+                popup = folium.Popup("<b>정류장명 : </b>" + f"{row['정류장명']}",max_width=300) # 마커 팝업에 표시할 정보 설정
+                tooltip = f"정류장번호: {row['정류장번호']}"
+                folium.Circle(location=[row.geometry.y, row.geometry.x],radius=10,fill=True,fill_opacity=0.8,popup=popup,tooltip=tooltip).add_to(m1)
     
 if bt_reset:
     with col1:
