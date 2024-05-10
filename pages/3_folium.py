124326bfebcd20d60a968a7821e65e77af7e04dd
Woody Jo <gusdn726@gmail.com>
2024-05-10 12:54:31 +0000

    modified:   pages/3_folium.py

--- pages/3_folium.py
+++ pages/3_folium.py
@@ -27,7 +27,7 @@ st.set_page_config(layout="wide")
 # 영역 그리는 지도
 m = folium.Map(location=[35.162943, 129.053097], zoom_start=11)
 Draw(export=True).add_to(m)
-
+# 지도 기능추가 (전체화면 기능)
 folium.plugins.Fullscreen(
     position="topright",
     title="전체화면",
@@ -35,30 +35,12 @@ folium.plugins.Fullscreen(
     force_separate_button=True,
 ).add_to(m)
 
-
-# 조회되는 지도
-# m1 = folium.Map(location=[35.176934,129.178065], zoom_start=6)
-
-
-
 # st_map = folium_static(m, width = 1100, height=500)
-
 df_bs_poly = None
 
-# with st.sidebar.form(key='search_form'):
-#     submit_button = st.form_submit_button(label='조회')
-#     uploaded_file = st.file_uploader("폴리곤파일(*.geojson)을 업로드해주세요.",type='geojson')
-#     if uploaded_file is not None:
-#         gdf = gpd.read_file(uploaded_file)
-#         if submit_button:
-#             bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
-#             df_bs_poly = pd.DataFrame(bs_poly.drop(columns='geometry'))
-#             m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=14)
-#             folium.GeoJson(data=gdf['geometry']).add_to(m1)
-#             # folium.GeoJson(data=gdf['geometry'],style_function=lambda feature: {'fillColor': 'yellow','color': 'yellow'}).add_to(m1)
-#             for idx, row in bs_poly.iterrows():
-#                 popup = f"Name: {row['정류장명']}"  # 마커 팝업에 표시할 정보 설정
-#                 folium.Circle(location=[row.geometry.y, row.geometry.x],radius=10,fill=True,fill_opacity=0.8,popup=popup).add_to(m1)
+
+###### 화면 구성
+
 
 st.header("타이틀 내용 입력")
 
@@ -68,8 +50,8 @@ with col1:
     with st.form("poly_form"):
          c1,c2 = st.columns([0.3,0.7])
          with c1:
-             button = st.form_submit_button(label="🔎  :green[조  회  하  기]")
-             b1 = st.form_submit_button("🔄  :blue[영역 재설정] ")
+             bt_search = st.form_submit_button(label="🔎  :green[조  회  하  기]")
+             bt_reset = st.form_submit_button("🔄  :blue[영역 재설정] ")
              with st.popover("📝  메뉴얼 보기"):
                  st.write("1. 지도 왼편 다각형(⬟) or 사각형(■) 선택")
                  st.write("2. 원하는 영역 그리기")
@@ -83,25 +65,25 @@ with col2:
     st.empty()
 
 
-if button:
+if bt_search:
     if uploaded_file is not None:
         gdf = gpd.read_file(uploaded_file)
-        if button:
-            bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
-            df_bs_poly = pd.DataFrame(bs_poly.drop(columns='geometry'))
-            m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
-            folium.plugins.Fullscreen(position="topright",title="전체화면",title_cancel="나가기",force_separate_button=True).add_to(m1)
-            folium.GeoJson(data=gdf['geometry'],).add_to(m1)
-            # folium.GeoJson(data=gdf['geometry'],style_function=lambda feature: {'fillColor': 'yellow','color': 'yellow'}).add_to(m1)
-            for idx, row in bs_poly.iterrows():
-                popup = folium.Popup("<b>정류장명 : </b>" + f"{row['정류장명']}",max_width=300) # 마커 팝업에 표시할 정보 설정
-                tooltip = f"정류장번호: {row['정류장번호']}"
-                folium.Circle(location=[row.geometry.y, row.geometry.x],radius=10,fill=True,fill_opacity=0.8,popup=popup,tooltip=tooltip).add_to(m1)
-if b1:
+        bs_poly = gpd.sjoin(gdf_bs,gdf,how='inner')
+        df_bs_poly = pd.DataFrame(bs_poly.drop(columns='geometry'))
+        m1 = folium.Map(location=[bs_poly.geometry.y.mean(),bs_poly.geometry.x.mean()], zoom_start=15)
+        folium.plugins.Fullscreen(position="topright",title="전체화면",title_cancel="나가기",force_separate_button=True).add_to(m1)
+        folium.GeoJson(data=gdf['geometry'],).add_to(m1)
+        # folium.GeoJson(data=gdf['geometry'],style_function=lambda feature: {'fillColor': 'yellow','color': 'yellow'}).add_to(m1)
+        for idx, row in bs_poly.iterrows():
+            popup = folium.Popup("<b>정류장명 : </b>" + f"{row['정류장명']}",max_width=300) # 마커 팝업에 표시할 정보 설정
+            tooltip = f"정류장번호: {row['정류장번호']}"
+            folium.Circle(location=[row.geometry.y, row.geometry.x],radius=10,fill=True,fill_opacity=0.8,popup=popup,tooltip=tooltip).add_to(m1)
+    
+if bt_reset:
     with col1:
         output = folium_static(m,width=1100,height=500)
 else:
-    if button:
+    if bt_search:
         with col1:
             st_m = folium_static(m1,width=1100,height=500)
             if df_bs_poly is not None:
