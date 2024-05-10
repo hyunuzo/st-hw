827906c1540e68b35b22755e9e626956c1e26492
Woody Jo <gusdn726@gmail.com>
2024-05-10 15:17:31 +0000

    modified:   pages/4_folium1.py

--- pages/4_folium1.py
+++ pages/4_folium1.py
@@ -59,7 +59,11 @@ with t1:
         a1, a2 = st.columns([0.9,0.1])
     
     with st.container(height= 100):
-         date1 = st.date_input("조회할 날짜",(datetime.date(2024,1,1),'today'))
+         st.wirte("조회할 날짜를 선택하세요")
+         st_dt = st.date_input("시작 날짜")
+         end_dt = st.date_input("끝 날짜")
+         st_time = st.time_input("시작 시간")
+         end_dt = st.time_input("끝 시간")
 
     with st.container(height= 210,border=None):
         b1, b2 = st.columns([0.3,0.7])
