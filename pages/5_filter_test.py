e42217774234d98aaabe96c297ee8e5915471113
Woody Jo <gusdn726@gmail.com>
2024-05-13 12:20:01 +0000

    modified:   pages/5_filter_test.py

--- pages/5_filter_test.py
+++ pages/5_filter_test.py
@@ -14,25 +14,26 @@ path_csv = 'img/도로교통공단_일자별 시군구별 교통사고 건수_20
 df = pd.read_csv(path_csv)
 
 sido = df['시도'].unique()
-sido1 = sido.append('전국')
 gungu = df['시군구'].unique()
-a = df[df['시도']== '부산']['시군구'].unique()
 
 def sido_gungu(data,select):
     filter_gungu = data[data['시도']== select]['시군구'].unique()
     return filter_gungu
 
 
-select_sido = st.selectbox('시도 선택',sido1)
-select_gungu = st.selectbox('시군구 선택',sido_gungu(df,select_sido))
+select_sido = st.selectbox('시도 선택',sido,index=None)
+select_gungu = st.selectbox('시군구 선택',sido_gungu(df,select_sido),index=None)
+
+if select_sido is not None:
+    if select_gungu is not None:
+        filter_df = df[(df['시도']== select_sido)&(df['시군구']== select_gungu)]
+    else:
+        filter_df = df[df['시도']== select_sido]
+else:
+    if select_gungu is not None:
+        filter_df = df[df['시군구']== select_gungu]
+    else:
+        filter_df = df
 
 
-
-# df1 = df[df['시도']==select_sido]
-
-
-
-
-st.write(select_sido)
-st.write(select_gungu)
-st.write(df)
+st.write(filter_df)
