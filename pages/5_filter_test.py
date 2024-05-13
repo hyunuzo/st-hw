fde42dae1c498d304746fbb054ee8561c6b9dba2
Woody Jo <gusdn726@gmail.com>
2024-05-13 12:00:38 +0000

    modified:   pages/5_filter_test.py

--- pages/5_filter_test.py
+++ pages/5_filter_test.py
@@ -23,7 +23,7 @@ def filter_select(select):
 select_sido = st.selectbox('시도 선택',sido)
 select_gungu = st.selectbox('시군구 선택',gungu)
 
-a = df[['시도']== '부산'].unique()
+a = df[df['시도']== '부산'].unique()
 
 st.write("표시")
 st.write(a)
