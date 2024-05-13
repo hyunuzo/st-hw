2e14d458e8267d9474ade416e800422db9e56fc7
Woody Jo <gusdn726@gmail.com>
2024-05-13 12:11:05 +0000

    modified:   pages/5_filter_test.py

--- pages/5_filter_test.py
+++ pages/5_filter_test.py
@@ -12,7 +12,7 @@ import datetime
 path_csv = 'img/도로교통공단_일자별 시군구별 교통사고 건수_20221231.csv'
 df = pd.read_csv(path_csv)
 
-sido = df['시도'].unique()
+sido = df['시도'].unique().append("전국")
 gungu = df['시군구'].unique()
 a = df[df['시도']== '부산']['시군구'].unique()
 
@@ -23,6 +23,12 @@ def sido_gungu(data,select):
 
 select_sido = st.selectbox('시도 선택',sido)
 select_gungu = st.selectbox('시군구 선택',sido_gungu(df,select_sido))
+
+
+
+# df1 = df[df['시도']==select_sido]
+
+
 
 
 st.write(select_sido)
