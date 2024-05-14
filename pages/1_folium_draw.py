885b7c8fb431d266d7584562f659f5ede3195ed5
Woody Jo <gusdn726@gmail.com>
2024-05-14 11:55:09 +0000

    modified:   pages/1_folium_draw.py

--- pages/1_folium_draw.py
+++ pages/1_folium_draw.py
@@ -12,6 +12,18 @@ from streamlit_folium import folium_static
 
 st.set_page_config(layout="wide")
 
+def _max_width_(prcnt_width:int = 75):
+    max_width_str = f"max-width: {prcnt_width}%;"
+    st.markdown(f""" 
+                <style> 
+                .reportview-container .main .block-container{{{max_width_str}}}
+                </style>    
+                """, 
+                unsafe_allow_html=True,
+    )
+
+_max_width_(80)
+
 m = folium.Map(location=[35.162943, 129.053097], zoom_start=11)
 Draw(export=True).add_to(m)
 
@@ -23,5 +35,3 @@ Draw(export=True).add_to(m)
 # with c2:
 #     st.write(st_folium(m, width=1000, height=500))
 st.write(st_folium(m, width=1000, height=500,returned_objects="last_active_drawing"))
-
-st.components.v1.html(m._repr_html_(), width=1100,height=500)
